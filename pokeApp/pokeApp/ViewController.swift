fe421892472776ed7efbda399705b1a42fa7f1d0
Faiq Adi <faiq.adiwibowo@gmail.com>
2025-11-04 15:38:55 +0700

    initiate file and add cocoapod

---
+++ pokeApp/pokeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokeApp
+//
+//  Created by Faiq Adi on 04/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
